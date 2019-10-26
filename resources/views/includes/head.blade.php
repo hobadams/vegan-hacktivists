bc46032164c344a1834c5657eec44088e8e62663
Hob Adams <hello@hobadams.com>
2019-10-26 15:25:31 +0100

    Fix bootstrap CDN link

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,4 +4,4 @@
 
 <title>Vegan Hacktivists - Code Challenge</title>
 
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
